9a6b50bdb32059acf818e510e44653052cb78f3d
Roucool_PvP <lucastag77@gmail.com>
2023-11-30 19:19:39 +0100

    Ça mache pas, mais ça plante pas

--- AStar.cc
+++ AStar.cc
@@ -22,8 +22,8 @@ AStar::AStar() {
 std::vector<Location> AStar::getPath() {
     std::vector<Location> path;
     // Penser à retourner le chemin dans le bon sens
-    for(std::vector<Node>::iterator it = _path.begin(); it != _path.end(); ++it) {
-        path.push_back(it->location);
+    for(std::vector<Node*>::iterator it = _path.begin(); it != _path.end(); ++it) {
+        path.push_back((*it)->location);
     }
     return path;
 }
@@ -35,8 +35,8 @@ void AStar::pathfind(const State& state, const Location& start, const Location& 
     _rows = state.rows;
     _cols = state.cols;
 
-    Node startNode = Node(start, end);
-    startNode.distanceFromStart = 0;
+    Node* startNode = _createNode(start, end);
+    startNode->distanceFromStart = 0;
     _toVisit.push_back(startNode);
 
     _pathfindLoop(state, end);
@@ -57,7 +57,7 @@ void AStar::_pathfindLoop(const State& state, const Location& end) {
         const_cast<State&>(state).bug << "0" << endl;
 
         // On récupère le meilleur noeud à visiter
-        Node bestNode = _getBestFromList(state);
+        Node* bestNode = _getBestFromList(state);
 
         const_cast<State&>(state).bug << "1" << endl;
 
@@ -69,37 +69,37 @@ void AStar::_pathfindLoop(const State& state, const Location& end) {
     }
 }
 
-void AStar::_visitNode(Node& node, const State& state, const Location& end) {
+void AStar::_visitNode(Node* node_ptr, const State& state, const Location& end) {
     // On valide la visite du noeud
-    node.explored = true;
+    node_ptr->explored = true;
     const_cast<State&>(state).bug << "1.0" << endl;
-    _toVisit.erase(std::find(_toVisit.begin(), _toVisit.end(), node));
-    _visited.push_back(node);
+    _toVisit.erase(std::find(_toVisit.begin(), _toVisit.end(), node_ptr));
+    _visited.push_back(node_ptr);
 
     const_cast<State&>(state).bug << "1.1" << endl;
 
     // Si la distance est trop grande, on arrête
-    if(_getNodeScore(node, state) > _maxDistance) {
+    if(_getNodeScore(node_ptr, state) > _maxDistance) {
         return;
     }
 
     // Si on est arrivé à la fin, on arrête
-    if(node.location == end) {
+    if(node_ptr->location == end) {
         const_cast<State&>(state).bug << "On est à la fin" << endl;
-        const_cast<State&>(state).bug << &node << " | " << node.previousNode << endl;
-        _maxDistance = node.distanceFromStart;
-        _validatePath(node, state);
+        const_cast<State&>(state).bug << &node_ptr << " | " << node_ptr->previousNode << endl;
+        _maxDistance = node_ptr->distanceFromStart;
+        _validatePath(node_ptr, state);
         return;
     }
 
     const_cast<State&>(state).bug << "1.2" << endl;
     // On ajoute les noeuds adjacents
-    _addAdjacentNodes(node, state);
+    _addAdjacentNodes(node_ptr, state);
 }
 
-void AStar::_addAdjacentNodes(Node& node, const State& state) {
-    Location location = node.location;
-    Node* previousNode = &node;
+void AStar::_addAdjacentNodes(Node* node_ptr, const State& state) {
+    Location location = node_ptr->location;
+    Node* previousNode = node_ptr;
     const_cast<State&>(state).bug << previousNode << endl;
 
     for(int d=0; d<TDIRECTIONS; d++) {
@@ -110,7 +110,7 @@ void AStar::_addAdjacentNodes(Node& node, const State& state) {
         if(_isLocationValid(adjacentLocation, state)) {
             // BUG: ICI EST LE PROBLEME
             // BUG: adjacentNode est désalloué en sortie de scope
-            Node adjacentNode = Node(adjacentLocation, _endLocation);
+            Node* adjacentNode = _createNode(adjacentLocation, _endLocation);
             const_cast<State&>(state).bug << &adjacentNode << endl;
 
             if(_getNodeScore(adjacentNode, state) > _maxDistance) {
@@ -121,12 +121,12 @@ void AStar::_addAdjacentNodes(Node& node, const State& state) {
             const_cast<State&>(state).bug << "1.2.1" << endl;
 
             // Je vois si je l'ai déjà visité
-            std::vector<Node>::iterator it = std::find(_visited.begin(), _visited.end(), adjacentNode);
+            std::vector<Node*>::iterator it = std::find(_visited.begin(), _visited.end(), adjacentNode);
             if(it != _visited.end()) {
                 // On regarde si on a trouvé un chemin plus court
-                if(it->distanceFromStart > node.distanceFromStart + 1) {
-                    it->distanceFromStart = node.distanceFromStart + 1;
-                    it->previousNode = previousNode;
+                if((*it)->distanceFromStart > node_ptr->distanceFromStart + 1) {
+                    (*it)->distanceFromStart = node_ptr->distanceFromStart + 1;
+                    (*it)->previousNode = previousNode;
                     _toVisit.push_back(*it);
                     _visited.erase(it);
                     const_cast<State&>(state).bug << "2.2.1" << endl;
@@ -141,9 +141,9 @@ void AStar::_addAdjacentNodes(Node& node, const State& state) {
             it = std::find(_toVisit.begin(), _toVisit.end(), adjacentNode);
             if(it != _toVisit.end()) {
                 // On regarde si on a trouvé un chemin plus court
-                if(it->distanceFromStart > node.distanceFromStart + 1) {
-                    it->distanceFromStart = node.distanceFromStart + 1;
-                    it->previousNode = previousNode;
+                if((*it)->distanceFromStart > node_ptr->distanceFromStart + 1) {
+                    (*it)->distanceFromStart = node_ptr->distanceFromStart + 1;
+                    (*it)->previousNode = previousNode;
                     // Vu que je modifie directement l'objet dans la liste, pas besoin de le réajouter
                     const_cast<State&>(state).bug << "2.2.2" << endl;
                 }
@@ -151,8 +151,8 @@ void AStar::_addAdjacentNodes(Node& node, const State& state) {
             }
 
             const_cast<State&>(state).bug << "1.2.3" << endl;
-            adjacentNode.previousNode = previousNode;
-            adjacentNode.distanceFromStart = node.distanceFromStart + 1;
+            adjacentNode->previousNode = previousNode;
+            adjacentNode->distanceFromStart = previousNode->distanceFromStart + 1;
 
             _toVisit.push_back(adjacentNode);
             const_cast<State&>(state).bug << "1.2.4" << endl;
@@ -161,9 +161,9 @@ void AStar::_addAdjacentNodes(Node& node, const State& state) {
 }
 
 // Retourne le meilleur noeud à visiter
-Node AStar::_getBestFromList(const State& state) {
-    Node bestNode = _toVisit[0];
-    for(std::vector<Node>::iterator it = _toVisit.begin(); it != _toVisit.end(); ++it) {
+Node* AStar::_getBestFromList(const State& state) {
+    Node* bestNode = _toVisit[0];
+    for(std::vector<Node*>::iterator it = _toVisit.begin(); it != _toVisit.end(); ++it) {
         if(_getNodeScore(*it, state) < _getNodeScore(bestNode, state)) {
             bestNode = *it;
         }
@@ -172,8 +172,8 @@ Node AStar::_getBestFromList(const State& state) {
 }
 
 // Retourne le score d'un noeud
-double AStar::_getNodeScore(const Node& node, const State& state) {
-    return node.distanceFromStart + _distanceToEnd(node, state);
+double AStar::_getNodeScore(const Node* node_ptr, const State& state) {
+    return node_ptr->distanceFromStart + _distanceToEnd(node_ptr, state);
 }
 
 // Fonction de reset entre deux pathfind
@@ -200,23 +200,29 @@ bool AStar::_isLocationValid(const Location& location, const State& state){
 }
 
 // Fonction qui génère le chemin à partir du noeud final
-void AStar::_validatePath(Node& node, const State& state) {
-    Node* currentNode = &node;
+void AStar::_validatePath(Node* node_ptr, const State& state) {
+    Node* currentNode = node_ptr;
     _path.clear();
     while(currentNode->distanceFromStart > 0) {
-        _path.push_back(*currentNode); // Le chemin commence par la fin, on l'inversera à la récupération.
+        _path.push_back(currentNode); // Le chemin commence par la fin, on l'inversera à la récupération.
         currentNode = currentNode->previousNode;
-        const_cast<State&>(state).bug << node.location.col << " ; " << node.previousNode->location.col << endl;
+        const_cast<State&>(state).bug << node_ptr->location.col << " ; " << node_ptr->previousNode->location.col << endl;
         const_cast<State&>(state).bug << currentNode << endl;
     }
 }
 
-double AStar::_distanceToEnd(const Node& node1, const State& state) { // Volé depuis State
-    Location loc1 = node1.location;
-    Location loc2 = node1.destination;
+double AStar::_distanceToEnd(const Node* node_ptr, const State& state) { // Volé depuis State
+    Location loc1 = node_ptr->location;
+    Location loc2 = node_ptr->destination;
     int d1 = abs(loc1.row-loc2.row),
         d2 = abs(loc1.col-loc2.col),
         dr = min(d1, state.rows-d1),
         dc = min(d2, state.cols-d2);
     return sqrt(dr*dr + dc*dc);
 };
+
+Node* AStar::_createNode(const Location& location, const Location& end) {
+    Node* node_ptr = new Node(location, end);
+    _every_node_ever_created.push_back(node_ptr);
+    return node_ptr;
+}
--- AStar.hh
+++ AStar.hh
@@ -12,27 +12,28 @@
 class AStar {
     private :
         // // TODO: Passage en pointeurs... :skull:
-        // std::vector<Node*> _toVisit;
-        // std::vector<Node*> _visited;
-        // std::vector<Node*> _path;
-        // std::vector<Node*> _every_node_ever_created;
+        std::vector<Node*> _toVisit;
+        std::vector<Node*> _visited;
+        std::vector<Node*> _path;
+        std::vector<Node*> _every_node_ever_created;
 
         // OLD
-        std::vector<Node> _toVisit;
-        std::vector<Node> _visited;
-        std::vector<Node> _path;
+        // std::vector<Node> _toVisit;
+        // std::vector<Node> _visited;
+        // std::vector<Node> _path;
         Location _endLocation;
         int _maxDistance;
         int _rows, _cols;
 
-        void _visitNode(Node& node, const State& state, const Location& end);
+        void _visitNode(Node* node, const State& state, const Location& end);
         bool _isLocationValid(const Location& location, const State& state);
-        double _distanceToEnd(const Node& node1, const State& state);
+        Node* _createNode(const Location& location, const Location& end);
+        double _distanceToEnd(const Node* node1, const State& state);
         void _pathfindLoop(const State& state, const Location& end);
-        double _getNodeScore(const Node& node, const State& state);
-        void _addAdjacentNodes(Node& node, const State& state);
-        void _validatePath(Node& node, const State& state);
-        Node _getBestFromList(const State& state);
+        double _getNodeScore(const Node* node, const State& state);
+        void _addAdjacentNodes(Node* node, const State& state);
+        void _validatePath(Node* node, const State& state);
+        Node* _getBestFromList(const State& state);
         void _reset();
 
     public :
