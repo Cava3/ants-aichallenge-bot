bfd23ebf8c1165af7e9d15f369f403c20cba988e
Roucool_PvP <lucastag77@gmail.com>
2024-01-14 23:08:43 +0100

    Comportement EAT

--- Ant.cpp
+++ Ant.cpp
@@ -73,7 +73,7 @@ int Ant::selectDirection(const State &state, double timeLimit)
 
 Location Ant::findClosestFood(const State &state)
 {
-    // TODO : calculer la distance en fonction du AStar, pas à vol d'oiseau
+    // Calculer la distance en fonction du AStar, pas à vol d'oiseau
     // Si pas de nourriture, on renvoie -1; -1
     Location closestFood = Location(-1, -1);
 
@@ -112,7 +112,7 @@ Location Ant::takeDecision(const State &state, double timeLimit)
 
                 // On trouve la Location opposée
                 Location oppositeLocation = Location(_position.row+diffY, _position.col+diffX);
-                if(state.grid[oppositeLocation.row][oppositeLocation.col].isWater) { // TODO: Remplacer par isLocationValid
+                if(state.grid[oppositeLocation.row][oppositeLocation.col].isWater) { // FIXME: Remplacer par isLocationValid
                     // Si on ne peut pas aller à l'endroit opposé, on essaye de pathfind en dehors de la vue.
                     oppositeLocation = Location(_position.row+diffY*5, _position.col-diffX*5);
                 }
@@ -145,7 +145,7 @@ Location Ant::takeDecision(const State &state, double timeLimit)
         while (randomLocation == Location(-1, -1)) {
             int randomRow = _position.row + (rand() % 10) - 5;
             int randomCol = _position.col + (rand() % 10) - 5;
-            if (!state.grid[randomRow][randomCol].isWater) { // TODO: Remplacer par isLocationValid
+            if (!state.grid[randomRow][randomCol].isWater) { // FIXME: Remplacer par isLocationValid
                 randomLocation = Location(randomRow, randomCol);
             }
         }
@@ -154,15 +154,59 @@ Location Ant::takeDecision(const State &state, double timeLimit)
         break;
 
     // Style de jeu du milieu de partie
-    // TODO : Chercher de la nourriture ou explorer
+    // Chercher de la nourriture ou explorer
     case PLAYSTYLE_EAT:
-        /* code */
+        // On cherche en priorité de la nourriture
+        Location closestFood = findClosestFood(state);
+        if (closestFood != Location(-1, -1)) {
+            if(_target == closestFood) {
+                _path.erase(_path.begin());
+                return _path[0];
+            } else {
+                setTarget(state, closestFood);
+                return _path[0];
+            }
+        }
+
+        // Si pas de nourriture, on explore
+        // Si on est encore loin de la destination actuelle, on continue
+        if (_path.size() > 3) {
+            _path.erase(_path.begin());
+            return _path[0];
+        }
+
+        // Sinon on explore un autre endroit
+        Location randomLocation = Location(-1, -1);
+        while (randomLocation == Location(-1, -1)) {
+            int randomRow = _position.row + (rand() % 10) - 5;
+            int randomCol = _position.col + (rand() % 10) - 5;
+            if (!state.grid[randomRow][randomCol].isWater) { // TODO: Remplacer par isLocationValid
+                randomLocation = Location(randomRow, randomCol);
+            }
+        }
+        setTarget(state, randomLocation);
+        return _path[0];
         break;
     
     // Style de jeu de la fin de partie
-    // TODO : Nourriture si très proche, sinon attaquer les nids
+    // Nourriture si très proche, sinon attaquer les nids
     case PLAYSTYLE_ANIHILATE:
-        /* code */
+        // On cherche en priorité de la nourriture
+        // TODO: Proche
+        Location closestFood = findClosestFood(state);
+        if (closestFood != Location(-1, -1)) {
+            if(_target == closestFood) {
+                _path.erase(_path.begin());
+                return _path[0];
+            } else {
+                setTarget(state, closestFood);
+                return _path[0];
+            }
+        }
+
+        // Si pas de nourriture, on attaque les nids
+        // TODO: Attaque des nids
+
         break;
     default:
         break;
